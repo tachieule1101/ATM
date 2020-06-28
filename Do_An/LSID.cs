48e345d1c46a99018efbf2eef99ca11eee7ca37c
trieuppp01230 <trieunguyencd17tt10@gmail.com>
2020-06-28 13:26:54 +0700

    update

---
+++ Do_An/LSID.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Do_An2
+{
+    class LSID
+    {
+        public string loaiGD, id, tG;
+        public int soTien;
+
+        public LSID()
+        {
+            loaiGD = null;
+            id = null;
+            tG = null;
+            soTien = 0;
+        }
+        public LSID(string loaiGD, string id, int soTien)
+        {
+            this.loaiGD = loaiGD;
+            this.id = id;
+            this.tG = tG;
+            this.soTien = soTien;
+        }
+    }
+}
